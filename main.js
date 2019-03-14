148e60f394aa1389927fc61e262c5fa4e05e8af6
hugo3 <halvarez0@ucol.mx>
2019-03-13 18:30:10 -0600

    Agenda 2

---
+++ main.js
@@ -0,0 +1,26 @@
+import Agenda from "./Agenda.js";
+import Employee from "./Employee.js";
+
+class Main {
+  constructor() {
+    let agenda = new Agenda(
+      document.querySelector("#agenda"),
+      document.querySelector("#info")
+    );
+
+    document.querySelector("#btnAdd").addEventListener("click", () => {
+      let name = document.querySelector("#name").value;
+      let email = document.querySelector("#email").value;
+      let sBirthday = document.querySelector("#birthday").value;
+      sBirthday = sBirthday.split("-");
+
+      let birthday = new Date(sBirthday[0], sBirthday[1] - 1, sBirthday[2]);
+
+      let employee = new Employee(name, email, birthday);
+
+      agenda.addEmployee(employee);
+    });
+  }
+}
+
+let m = new Main();
